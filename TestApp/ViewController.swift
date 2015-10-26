90cc43c2c17a0e9b54c6ce730702376abd3c17ea
Kaushik Barman <kaushik.barman@tcs.com>
2015-10-26 14:43:08 +0530

    Initial

---
+++ TestApp/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by 01HW664134 on 20/07/15.
+//  Copyright (c) 2015 TCS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var txtField:UITextField!;
+    @IBOutlet var slider:UISlider?;
+    @IBOutlet weak var btn: UIButton!;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func actionOnSlide(sender: AnyObject)
+    {
+        println("Value changed on slide");
+    }
+
+    @IBAction func actionOnButton(sender: AnyObject)
+    {
+        println("Button pressed !!!!");
+    }
+    
+}
+

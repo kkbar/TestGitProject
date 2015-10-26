56bdb1d1d17dded7d6abc42f5814d0b6de1924a8
Supratim Patra <supratim.patra@tcs.com>
2015-10-26 15:17:12 +0530

    New View Ctrl added

---
+++ TestApp/WelcomeViewCtrl.swift
@@ -0,0 +1,13 @@
+//
+//  WelcomeViewCtrl.swift
+//  TestApp
+//
+//  Created by 01HW612323 on 26/10/15.
+//  Copyright © 2015 TCS. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewCtrl: UIViewController {
+
+}
